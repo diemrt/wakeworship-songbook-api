d7cf8dafbe16362bd5081cdd37fdcccc0cb2a364
diegomartignoni <m.s.d.d.diego@gmail.com>
2023-07-20 02:12:08 +0200

    backlog-api-feat: add song request

---
+++ Songbook.Domain/Requests/v1/AddEditSongRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Songbook.Domain.Response.v1.Common;
+
+namespace Songbook.Domain.Requests.v1
+{
+	public class AddEditSongRequest
+	{
+		public required string Title { get; set; }
+		public required int Capo { get; set; }
+		public required GenericItemResponse<string>  Key { get; set; }
+		public required string Song { get; set; }
+
+        //verse)Questo è il[c] testo di una[a] canzone[none];Cosa te ne [g]pare[none]#chours)Bello![g]
+    }
+}
+
